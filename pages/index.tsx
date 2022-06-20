cce3bb219e165815061c4055c6613ef0e489b87c
Alex <daftpy@gmail.com>
2022-06-19 23:30:00 -0500

    Updated index page to reflect the intended design.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import BaseLayout from '../components/core/BaseLayout'
 import PaperPanel from '../components/PaperPanel'
+import styled from 'styled-components'
+import Image from 'next/image'
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  border-radius: 2px;
+  background: url('/resources/navbar/glitter_bg.png'); 
+  width: fit-content;
+  margin: 5rem 2rem;
+  @media (min-width: 768px) {
+    margin: 5rem auto;
+  }
+`
 
 const Home: NextPage = () => {
   return (
@@ -13,10 +26,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-      <PaperPanel>
-        <p>hello paper</p>
-        <p>hello</p>
-      </PaperPanel>
+        <Wrapper>
+          <Image height={825} width={900} src="/resources/photos/monster_art.png" />
+        </Wrapper>
       </main>
     </BaseLayout>
     )
