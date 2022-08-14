80da6b572bad8f489bef296d201c58abcd9cea38
Alex <daftpy@gmail.com>
2022-08-14 02:36:02 -0500

    Updated tour page. Added event.

--- pages/tour/index.tsx
+++ pages/tour/index.tsx
@@ -1,15 +1,20 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import styled from "styled-components";
 import BaseLayout from "../../components/core/BaseLayout";
 
 const TourWrapper = styled.div`
-  margin: 8rem 10%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   font-size: 1.8rem;
   color: #FEFEFE;
+  min-width: 100%;
+  flex-direction: column;
+
   @media (min-width: 768px) {
-    margin: 8rem 30%;
+    padding: 0 3rem;
+    flex-direction: row;
   }
 `
 
@@ -30,19 +35,37 @@ const Background = styled.div`
   background-size: 280px, 300px;
   background-origin: border-box;
 `
+const TourInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const VenueName = styled.div`
+  font-weight: bold;
+`
 
 const Tour: NextPage = () => {
   return (
     <BaseLayout>
       <Background>
-          <p style={{textAlign: 'center', color: 'white', fontSize: '1.5rem'}}>
+          <p style={{textAlign: 'center', color: 'white', fontSize: '1.5rem', marginBottom: '3rem'}}>
             For bookings contact ham@lowsarecords.com
           </p>
+          <Link href="https://themoroccan.com/event/yards4le-ezra-kaden-jomie-hamilton/">
+            <a>
+              <TourWrapper>
+                <TourInfo>
+                  <VenueName>Moroccan Lounge</VenueName>
+                  <TourDate>Sept 14th</TourDate>
+                </TourInfo>
+                <TourTickets>
+                  TICKETS
+                </TourTickets>
+              </TourWrapper>
+            </a>
+          </Link>
         {/* <TourWrapper>
-          <TourDate>May 2nd</TourDate>
-          <TourTickets>TICKETS</TourTickets>
-        </TourWrapper>
-        <TourWrapper>
           <TourDate>May 3rd</TourDate>
           <TourTickets>TICKETS</TourTickets>
         </TourWrapper>
