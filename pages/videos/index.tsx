8ce0ecc61d70c70c15bc2a612574a89abd250e58
Alex <daftpy@gmail.com>
2022-09-29 22:50:12 -0500

    Removed iframe causing error.

--- pages/videos/index.tsx
+++ pages/videos/index.tsx
@@ -75,6 +75,4 @@ const Videos: NextPage = () => {
   )
 }
 
-<iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
 export default Videos;
